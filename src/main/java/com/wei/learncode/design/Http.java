fc18174a631fca0b34c3e562161ac453da1128dd
skyemin <1515054372@qq.com>
2020-02-25 18:34:09 +0800

    面向接口编程

---
+++ src/main/java/com/wei/learncode/design/Http.java
@@ -0,0 +1,12 @@
+package com.wei.learncode.design;
+
+/**
+ * @Author: skye
+ * @Date: 2020/2/25 16:20
+ * @Description:
+ * @Version:1.0
+ */
+public interface Http {
+
+    public void get();
+}
---
+++ src/main/java/com/wei/learncode/design/OkHttp.java
@@ -0,0 +1,14 @@
+package com.wei.learncode.design;
+
+/**
+ * @Author: skye
+ * @Date: 2020/2/25 16:21
+ * @Description:
+ * @Version:1.0
+ */
+public class OkHttp implements Http{
+    @Override
+    public void get() {
+        System.out.println("okhttp...");
+    }
+}
