d5728bef0c950dc83ca802d6a70b8cbe8aa1ac7b
skyemin <1515054372@qq.com>
2020-08-26 15:48:11 +0800

    算法补充

---
+++ src/main/java/com/wei/learncode/begin/LowestCommonAncestor.java
@@ -0,0 +1,23 @@
+package com.wei.learncode.begin;
+
+/**
+ * @Author: skye
+ * @Date: 2020/8/24 11:03
+ * @Description:
+ * @Version:1.0
+ */
+public class LowestCommonAncestor {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+
+}
